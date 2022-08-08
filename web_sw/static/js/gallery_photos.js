48fbc16beb0e2da52ca4cd144b086652aaa2a2cd
yoniks <89047399+yoniks@users.noreply.github.com>
2022-08-08 15:55:00 +0300

    Update gallery_photos.js

--- web_sw/static/js/gallery_photos.js
+++ web_sw/static/js/gallery_photos.js
@@ -26,10 +26,13 @@ $( document ).ready(function() {
                            document.querySelectorAll(".add-item-color")[i].style.border="0.5px solid #000000";
                         }
 
-                        doc.color = this.style.backgroundColor;
+                        
                         document.querySelectorAll(".add-item-color")[i].style.border="1px solid #000000";
                          document.querySelectorAll(".add-item-color")[i].style.width = "30px";
                          document.querySelectorAll(".add-item-color")[i].style.height = "30px";
+                       
+                        doc.color = this.style.backgroundColor;
+                        doc.url_img = document.querySelectorAll("#search-img")[i].textContent;
 
                                // alert(doc.color);
 
@@ -38,7 +41,7 @@ $( document ).ready(function() {
 
 
 
-
+   
           //send the list to python
           document.querySelector("#add-item-cart").addEventListener("click",function(){
             //we get ref of size color  add-item-sku
@@ -46,13 +49,11 @@ $( document ).ready(function() {
              doc.sku = document.querySelector("#add-item-sku").textContent;
              doc.price = parseInt(document.querySelector("#add-item-price").textContent);
               doc.title = document.querySelector("#add-item-title").textContent;
-              const test = document.querySelector("#search-img").textContent;
-              alert(doc.price);
 
-              alert(doc.color)
+
               temp_list.push(doc)
 
-           if(doc.sku[0]=='D' && doc.sku[1]=='R'){
+           if(doc.sku[0]=='D' && doc.sku[1]=='R' && doc.color.length>0 && doc.size.length>0){
                $.ajax({
                   type: "POST",
                   url: "/home/dresses/to",
@@ -65,25 +66,59 @@ $( document ).ready(function() {
                     start =  setTimeout(function(){// play delay 1 second
                      document.querySelector("#add-item-cart").innerHTML='ADD TO CARD';
                      document.querySelector("#add-item-size").innerHTML='SIZE';
+
                      clearTimeout(start);
 
                     },1000);
 
                 }
+
                 });//close ajax
 
-            }else if(doc.sku[0]=='S' && doc.sku[1]=='W'){
+            }else if(doc.sku[0]=='S' && doc.sku[1]=='W' && doc.color.length>0 && doc.size.length>0){
+
+
+                  $.ajax({
+                  type: "POST",
+                  url: "/home/swimwear/to",
+                  data: JSON.stringify(temp_list),
+                  contentType: "application/json",
+                  dataType: 'json'
+               }).done(function(data){
+                if(data.dresses=='ok'){
+                     document.querySelector("#add-item-cart").innerHTML='Added';
+                    start =  setTimeout(function(){// play delay 1 second
+                     document.querySelector("#add-item-cart").innerHTML='ADD TO CARD';
+                     document.querySelector("#add-item-size").innerHTML='SIZE';
+                     clearTimeout(start);
+
+                    },1000);
+                }
+
+                });//close ajax
 
 
 
 
+            }else{
+             alert('Please Select Color and  Size');
             }
 
+             if(doc.color.length>0 && doc.size.length>0){// clean anyway if the item sent or not
+                doc.color="";
+                doc.size ="";
+                doc.url_img="";
+              let btn_colors = document.querySelectorAll(".add-item-color").length;
+              for(let i=0;i<btn_colors;i++){
+                           document.querySelectorAll(".add-item-color")[i].style.width = "25px";
+                           document.querySelectorAll(".add-item-color")[i].style.height = "25px";
+                           document.querySelectorAll(".add-item-color")[i].style.border="0.5px solid #000000";
+                        }
+              }
 
 
 
-
-         });//CLOSE click function
+         });//CLOSE add  function
 
 
 
