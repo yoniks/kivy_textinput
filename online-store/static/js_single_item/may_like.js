b61245cb1a8919e20984f565923d4949efc045ef
yoniks <89047399+yoniks@users.noreply.github.com>
2022-09-12 11:16:17 +0300

    Add files via upload

---
+++ online-store/static/js_single_item/may_like.js
@@ -0,0 +1,64 @@
+
+
+
+
+$( document ).ready(function() {
+
+
+
+
+
+        const img_cards= document.querySelectorAll("#may-like-items").length
+          for(let i=0;i<img_cards;i++){
+
+            document.querySelectorAll("#may-like-items")[i].addEventListener("click",function(){
+            //disabled=true;
+            document.querySelectorAll("#may-like-items").disabled=true;
+            const sku = document.querySelectorAll("#Items-Sku-Card")[i].textContent;
+
+
+               ref = $.ajax({
+                  type: "POST",
+                  url: "/home/products/display/item",
+                  data: {sku:sku}
+               });
+               ref.done(function(data){
+                    $('#refresh-single-item').fadeOut(500).fadeIn(500);
+                    $('#refresh-single-item').html(data);
+                    document.querySelectorAll("#may-like-items").disabled=false;
+
+               });
+
+
+          });
+       }
+
+//show cart
+
+
+
+//email
+      document.querySelector("#form-submit").addEventListener("click",function(){
+           const subscript_email = document.querySelector('input[name="email-subscript"]');
+
+             $.ajax({
+             type: "POST",
+             url: "/home/send/email/to/subscript",
+             data: JSON.stringify(subscript_email.value),
+             contentType: "application/json",
+             dataType: 'json'
+           }).done(function(data){
+              if(data.received=="sent"){
+                  alert("received");
+              }
+
+           });
+      });
+
+
+
+
+
+
+
+   });
